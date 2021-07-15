c669dd532451fe5bebb857ecf8a7dce91df012d5
hackycy <qa894178522@qq.com>
2021-07-15 17:22:23 +0800

    chore：lerna init

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,16 @@ module.exports = {
     quotes: [2, 'single'],
     semi: ['error', 'never'],
     'comma-dangle': ['error', 'never']
-  }
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.js'],
+      env: {
+        jest: true
+      },
+      rules: {
+        'node/no-extraneous-require': 'off'
+      }
+    }
+  ]
 }
