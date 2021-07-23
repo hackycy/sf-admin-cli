10742bb7fa5e8af2cc9719713f2dce71ea128839
hackycy <qa894178522@qq.com>
2021-07-23 10:26:35 +0800

    chore：add fs-extra

--- core/utils/index.js
+++ core/utils/index.js
@@ -5,9 +5,11 @@ const chalk = require('chalk')
 const log = require('./lib/log')
 const npm = require('./lib/npm')
 const spinner = require('./lib/spinner')
+const fs = require('fs-extra')
 
 exports.npm = npm
 exports.spinner = spinner
 exports.log = log
 exports.semver = semver
 exports.chalk = chalk
+exports.fs = fs
