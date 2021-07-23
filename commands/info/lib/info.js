297e486011b4e2e5d2fe6c8234d1ca00de1e89d0
hackycy <qa894178522@qq.com>
2021-07-23 18:13:03 +0800

    feat：add show spinner & npmPackages

--- commands/info/lib/info.js
+++ commands/info/lib/info.js
@@ -1,14 +1,16 @@
 'use strict'
 
 const envinfo = require('envinfo')
-const { log, chalk } = require('@sfadminltd/utils')
+const { log, chalk, spinner } = require('@sfadminltd/utils')
 
 async function getEnvInfo() {
-	console.log(chalk.cyan(chalk.bold('\nEnvironment Info:')))
+  console.log(chalk.cyan(chalk.bold('\nEnvironment Info:')))
+	spinner.logWithSpinner('Loading...')
   const info = await envinfo.run(
     {
       System: ['OS', 'CPU'],
       Binaries: ['Node', 'Yarn', 'npm'],
+      npmPackages: '/**/{typescript,*vue*,@vue/*/,@nestjs/*/,@midwayjs/*/}',
       npmGlobalPackages: ['@sfadminltd/cli']
     },
     {
@@ -17,11 +19,13 @@ async function getEnvInfo() {
       fullTree: true
     }
   )
+  spinner.stopSpinner(false)
 	console.log(info)
 }
 
 module.exports = (...args) => {
   return getEnvInfo(...args).catch((err) => {
+    spinner.stopSpinner(false)
     log.error(`get env info error, reason: ${err}`)
   })
 }
