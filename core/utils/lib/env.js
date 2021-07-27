7d4185bd892b6da6bd4ac8c504064b2c50ce5dfc
hackycy <qa894178522@qq.com>
2021-07-27 16:24:29 +0800

    feat：add hasDocker

--- core/utils/lib/env.js
+++ core/utils/lib/env.js
@@ -4,6 +4,7 @@ const { execSync } = require('child_process')
 
 let _hasYarn
 let _hasGit
+let _hasDocker
 
 exports.hasYarn = () => {
   if (_hasYarn != null) {
@@ -38,4 +39,16 @@ exports.hasProjectGit = (cwd) => {
     result = false
   }
   return result
+}
+
+exports.hasDocker = () => {
+  if (_hasDocker != null) {
+    return _hasDocker
+  }
+  try {
+    execSync('docker --version', { stdio: 'ignore' })
+    return (_hasDocker = true)
+  } catch (e) {
+    return (_hasDocker = false)
+  }
 }
