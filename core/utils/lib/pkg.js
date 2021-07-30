263717f51a1d890dc222e25683e7b87326644d68
hackycy <qa894178522@qq.com>
2021-07-30 16:38:07 +0800

    fix：fix read pkg using

--- core/utils/lib/pkg.js
+++ core/utils/lib/pkg.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const { readPackageSync } = require('read-pkg')
+const readPkg = require('read-pkg')
 const fs = require('fs')
 const path = require('path')
 
 exports.resolvePkg = function(cwd) {
   if (fs.existsSync(path.join(cwd, 'package.json'))) {
-    return readPackageSync({ cwd })
+    return readPkg.sync({ cwd })
   }
   return {}
 }
