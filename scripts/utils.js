a3173de62966cc0ec62b64a04487b52be80dcee4
hackycy <qa894178522@qq.com>
2021-07-31 18:15:28 +0800

    feat：add complete log

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,13 +2,14 @@
 
 const axios = require('axios')
 const fs = require('fs')
+const path = require('path')
 const log = require('npmlog')
 
+// 初始化npmlog配置
 function initLog() {
   log.heading = 'sf tool'
 }
 
-// 初始化npmlog配置
 initLog()
 
 const MOCK_UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'
@@ -16,7 +17,7 @@ const MOCK_UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537
 /**
  * 下载文件
  * @param {string} fileUrl 下载文件链接
- * @param {string} output 输出的文件路径
+ * @param {string} output 输出的文件路径，必须为文件路径不可以为文件夹路径
  */
 async function downloadFile(fileUrl, output) {
   log.info('正在读取链接...')
@@ -34,20 +35,24 @@ async function downloadFile(fileUrl, output) {
     const totalLength = headers['content-length']
     let downloadLength = 0
     
+    // 启用进度条
     log.enableProgress()
 
     const writer = fs.createWriteStream(output)
     data.pipe(writer)
     data.on('data', (chunk) => {
-      log.showProgress('downloading...', (downloadLength += chunk.length) / totalLength)
+      log.showProgress('下载中...', (downloadLength += chunk.length) / totalLength)
     })
     writer.on('error', (err) => {
       writer.close()
+      // 关闭进度条
       log.disableProgress()
       reject(err)
     })
     writer.on('close', () => {
+      // 关闭进度条
       log.disableProgress()
+      log.info(`文件${path.basename(output)}已下载完成`)
       resolve()
     })
   })
