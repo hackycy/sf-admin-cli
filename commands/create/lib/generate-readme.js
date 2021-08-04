9f597f93fcdfd97a76d1d8d87b0d5f1b2bda1e9e
小小源逼 <qa894178522@qq.com>
2021-08-04 22:16:30 +0800

    feat：remove config readme text

--- commands/create/lib/generate-readme.js
+++ commands/create/lib/generate-readme.js
@@ -7,38 +7,22 @@
  */
 module.exports = function generateReadme(type, name, packageManager) {
   return [
-    `# ${name}-${type}\n`,
+    `# ${name}-${type}`,
+    '',
+    '** 使用文档: ** https://blog.si-yee.com/sf-admin-cli/',
+    '',
     '## 项目初始化',
     '```',
     `${packageManager} install`,
     '```',
     '',
-    ...(type === 'server'
-      ? [
-          '## 系统配置\n',
-          '### 管理员账号密码\n',
-          '|   账号    |  密码  |    权限    |',
-          '| :-------: | :----: | :--------: |',
-          '| rootadmin | 123456 | 超级管理员 |',
-          '',
-          '### 初始化数据库\n',
-          '手动导入sql文件下的所有sql文件',
-          '',
-          '### 更改开发环境配置\n',
-          '修改对应运行环境下的配置文件`config.${env}.ts`',
-          ''
-        ]
-      : []),
     '## 运行项目',
     '```',
     `${packageManager} ${packageManager !== 'yarn' ? 'run ' : ''}dev`,
     '```',
     '',
-    '# 版权声明\n',
-    '> 请尊重原创，原项目遵循 **MIT协议**',
-    '> 原项目链接：',
-    '> - [sf-midway-admin](https://github.com/hackycy/sf-midway-admin)',
-    '> - [sf-nest-admin](https://github.com/hackycy/sf-nest-admin)',
-    '> - [sf-vue-admin](https://github.com/hackycy/sf-vue-admin)'
+    '# 版权声明',
+    '',
+    '> 请尊重原创，项目遵循 **MIT协议**'
   ].join('\n')
 }
