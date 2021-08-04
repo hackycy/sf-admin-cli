1ca0b5d2d2b9db341e6ee120173b680e3bb510fb
hackycy <qa894178522@qq.com>
2021-08-04 15:37:32 +0800

    docs：add gh page script

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,19 +3,15 @@
 const { log } = require('./utils')
 const path = require('path')
 const fs = require('fs-extra')
-const chalk = require('chalk')
 
 /**
  * 清理临时目录
  */
 async function clean() {
   const tempDirPath = path.resolve(__dirname, '../', 'temp')
-  log.info('clean', `正在清理目录：${chalk.yellow(tempDirPath)}`)
+  // clean
+  log.info(`正在清理目录：${tempDirPath}`)
   await fs.remove(tempDirPath)
-
-  const docsDistPath = path.resolve(__dirname, '../', 'docs', '.vuepress', 'dist')
-  log.info('clean', `正在清理目录：${chalk.yellow(docsDistPath)}`)
-  await fs.remove(docsDistPath)
 }
 
 clean().catch(e => {
--- scripts/gh-page.js
+++ scripts/gh-page.js
@@ -5,9 +5,8 @@ const execa = require('execa')
 const fs = require('fs-extra')
 const path = require('path')
 
-const TEMP_DIR_PATH = path.resolve(__dirname, '../', 'temp')
-const GH_PAGE_BRANCH = 'gh-page'
-const GH_PAGE_DIR = path.join(TEMP_DIR_PATH, GH_PAGE_BRANCH)
+const GH_PAGE_BRANCH = 'sf-admin-cli-gh-page'
+const GH_PAGE_DIR = path.join(__dirname, '../', '../', GH_PAGE_BRANCH)
 const DOCS_DIST_PATH = path.resolve(__dirname, '../', 'docs', '.vuepress', 'dist')
 
 /**
@@ -21,8 +20,8 @@ async function checkoutghpage() {
   await execa('npm', ['run', 'docs:build'], { stdio: 'inherit' })
 
   // 临时文件夹
-  fs.ensureDirSync(TEMP_DIR_PATH)
-  fs.emptyDirSync(TEMP_DIR_PATH)
+  fs.ensureDirSync(GH_PAGE_DIR)
+  fs.emptyDirSync(GH_PAGE_DIR)
 
   // git clone
   log.info('git', '执行git clone...')
@@ -33,34 +32,28 @@ async function checkoutghpage() {
       '-b',
       GH_PAGE_BRANCH,
       'https://github.com/hackycy/sf-admin-cli.git',
-      GH_PAGE_BRANCH
+      '.'
     ],
-    { cwd: TEMP_DIR_PATH, stdio: 'inherit' }
+    { cwd: GH_PAGE_DIR, stdio: 'inherit' }
   )
-  
-  log.info('gh-page', '清理原工作空间...')
-  const originDirList = fs.readdirSync(GH_PAGE_DIR)
-  for (let i = 0; i < originDirList.length; i++) {
-    if (!originDirList[i] === '.git') {
-      fs.removeSync(path.join(GH_PAGE_DIR, originDirList[i]))
-    }
-  }
 
   log.info('gh-page', '拷贝构建目录...')
-  fs.moveSync(DOCS_DIST_PATH, GH_PAGE_DIR, { overwrite: true })
+  fs.copySync(DOCS_DIST_PATH, GH_PAGE_DIR, { overwrite: true })
 
   log.info('git', '提交中...')
   const options = { stdio: 'inherit', cwd: GH_PAGE_DIR }
   await execa('git', ['add', '-A'], options)
   await execa('git', ['commit', '-m', 'update docs', '--no-verify'], options)
-  await execa('git', ['push', '-u', '-f'], options)
+  await execa('git', ['push', 'origin', GH_PAGE_BRANCH, '-f'], options)
   log.info('git', '文档已更新...')
 
   log.info('gh-page', '清理构建...')
-  await execa('npm', ['run', 'clean'], options)
+  await execa('npm', ['run', 'clean'])
+  await fs.remove(GH_PAGE_DIR)
 }
 
 checkoutghpage().catch((err) => {
+  fs.removeSync(GH_PAGE_DIR)
   log.error('gh-page', err)
   process.exit(1)
 })
