98db142f6a8faef19eb0ef03da2cfb57ace1fab0
hackycy <qa894178522@qq.com>
2021-08-04 15:50:05 +0800

    feat：add clean vuepress dist

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,15 +3,19 @@
 const { log } = require('./utils')
 const path = require('path')
 const fs = require('fs-extra')
+const chalk = require('chalk')
 
 /**
  * 清理临时目录
  */
 async function clean() {
   const tempDirPath = path.resolve(__dirname, '../', 'temp')
-  // clean
-  log.info(`正在清理目录：${tempDirPath}`)
+  log.info('clean', `正在清理目录：${chalk.yellow(tempDirPath)}`)
   await fs.remove(tempDirPath)
+
+  const docsDistPath = path.resolve(__dirname, '../', 'docs', '.vuepress', 'dist')
+  log.info('clean', `正在清理目录：${chalk.yellow(docsDistPath)}`)
+  await fs.remove(docsDistPath)
 }
 
 clean().catch(e => {
