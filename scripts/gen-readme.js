043ae1553fdccb2246295f9618f14643f9ddd0fa
小小源逼 <qa894178522@qq.com>
2021-08-14 22:43:32 +0800

    feat：impl gen readme script

--- scripts/gen-readme.js
+++ scripts/gen-readme.js
@@ -1,13 +1,31 @@
 'use strict'
 
 const { log } = require('./utils')
+const fs = require('fs-extra')
+const path = require('path')
+const chalk = require('chalk')
+
+// package文件夹
+const MODULES = ['commands', 'core']
 
 /**
  * 同步生成package下的README
  */
-async function genDocs() {}
+async function genReadme() {
+  const ROOT_README = fs.readFileSync(path.resolve(__dirname, '..', 'README.md'))
 
-genDocs().catch(err => {
+  for (let i = 0; i < MODULES.length; i++) {
+    const moduleChildDir = path.resolve(__dirname, '..', MODULES[i])
+    const pkgNames = fs.readdirSync(moduleChildDir)
+    for (let j = 0; j < pkgNames.length; j++) {
+      const pkgDir = path.resolve(moduleChildDir, pkgNames[j])
+      fs.writeFileSync(path.join(pkgDir, 'README.md'), ROOT_README)
+      log.info('gen-md', `已生成${chalk.yellow(pkgDir)}下的README.md文档`)
+    }
+  }
+}
+
+genReadme().catch(err => {
   log.error('gh-page', err)
   process.exit(1)
 })
